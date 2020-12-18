759a9f4b936832fb4f42063ce800ced42628bbcb
peakacode <peakacode@gmail.com>
2020-12-18 11:16:39 +0800

    14 Component Communication Example 2 -Event Dispatcher copyNamed Slots in a Nutshell
    My Daily Coding Journey with Jeffrey Way - Larcaasts

---
+++ 14 Component Communication Example 2 -Event Dispatcher copyNamed Slots in a Nutshell/app.js
@@ -0,0 +1,34 @@
+Vue.component('modal', {
+  /* html */
+  template: ` 
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      
+      <div class="modal-card">
+        
+        <header class="modal-card-head">
+          <p class="modal-card-title">
+            <slot name="header"></slot>
+          </p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+      
+        <section class="modal-card-body">
+          <slot name="section"></slot>
+        </section>
+      
+        <footer class="modal-card-foot">
+        <slot name="footer">
+          <a class="button is-primary">Okay</a>
+        </slot>
+        </footer>
+      
+      </div>
+    </div>
+  `,
+});
+
+new Vue({
+    el: '#root',
+
+});
